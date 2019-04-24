eb2ba621ae57c5f0907b9d2da5d6279035130952
F1r3byte <f1r3byte@yandex.com>
2019-04-24 23:49:28 +0200

    Updates

---
+++ precache-manifest.4186b06f763672db92d568c877180c29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066d8e182978091e15c8a82a0e09c5bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b891ef66570fa5b1b223",
+    "url": "./static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c9c6526696fd71f3fe65",
+    "url": "./static/css/main.1f73cb24.chunk.css"
+  },
+  {
+    "revision": "b891ef66570fa5b1b223",
+    "url": "./static/js/2.93ed5dd5.chunk.js"
+  },
+  {
+    "revision": "c9c6526696fd71f3fe65",
+    "url": "./static/js/main.74c0251d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
